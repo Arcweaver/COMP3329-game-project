bd37ea75d46c0f023d44a2c457a90864cfb469a7
Anson <dsadas7000@gmail.com>
2025-03-26 15:05:07 +0800

    fix the event warning

--- Assets/Scripts/UI/SkillMenuItemScript.cs
+++ Assets/Scripts/UI/SkillMenuItemScript.cs
@@ -22,6 +22,7 @@ public class SkillMenuItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     public void SetIcon()
     {
         gameObject.GetComponent<Image>().sprite = skill.icon;
+        gameObject.GetComponentInChildren<TMP_Text>().text = "";
     }
 
     // Show the tooltip with info of the skill
