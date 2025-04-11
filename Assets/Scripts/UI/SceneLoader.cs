77653ad1290727ff633774fa8ee46592e5f63fa4
Eylssca <125619110+Eylssca@users.noreply.github.com>
2025-04-12 01:45:34 +0800

    basic game flow implemented in order & modified CompletionUI and StorylineScene
    only need to wait for the storyline.
    just copy and paste to TextAutoScroll.cs once we get the story.

--- Assets/Scripts/UI/SceneLoader.cs
+++ Assets/Scripts/UI/SceneLoader.cs
@@ -3,13 +3,40 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadGameScene()
+    public void StartGame()
     {
-        SceneManager.LoadScene("Level1"); // Replace "GameScene" with the name of your real game scene.
+        PlayerPrefs.SetInt("NextLevelIndex", 1); // Set for Level 1
+        SceneManager.LoadScene("StorylineScene"); // Load Level 1 story
     }
 
-    public void LoadStorylineScene()
+    public void LoadNextScene()
     {
-        SceneManager.LoadScene("StorylineScene"); // Replace "GameScene" with the name of your real game scene.
+        int nextLevelIndex = PlayerPrefs.GetInt("NextLevelIndex", 1); // Default to Level1 if not set
+        string nextScene;
+
+        if (nextLevelIndex == 1)
+        {
+            nextScene = "Level1";
+        }
+        else if (nextLevelIndex == 2)
+        {
+            nextScene = "Level2";
+        }
+        else if (nextLevelIndex == 3)
+        {
+            nextScene = "Level3";
+        }
+        else
+        {
+            // Final level completed, return to menu
+            nextScene = "GameMenu";
+        }
+
+        SceneManager.LoadScene(nextScene);
+    }
+
+    public void LoadGameMenu()
+    {
+        SceneManager.LoadScene("GameMenu");
     }
 }
