233fa168df12576db1ff3e61c4900c3c7d1701c3
Arcweaver <yanmanlai@gmail.com>
2025-04-21 15:32:38 +0800

    hammer for purge + rating score

--- Assets/Scripts/units&levels/levels/Level 1/lvl1_ObjTracker.cs
+++ Assets/Scripts/units&levels/levels/Level 1/lvl1_ObjTracker.cs
@@ -11,8 +11,38 @@ public class lvl1_ObjTracker : GameObjectiveTracker
 
     public override string ParseObjectiveText()
     {
+        // score and rating
+        int score = 0;
+        string rating;
+        if (!isHitBySkill) score += 30;
+        score += player.currentHealth;
+
+        switch (score)
+        {
+            case >= 120:
+                rating = "S";
+                break;
+            case >= 80:
+                rating = "A";
+                break;
+            case >= 60:
+                rating = "B";
+                break;
+            case >= 40:
+                rating = "C";
+                break;
+            case >=20:
+                rating = "D";
+                break;
+            default:
+                rating = "D";
+                break;
+
+        }
+
         return $"Player Hits: {playerHits}\n" +
             $"Player HP: {player.currentHealth}/{player.maxHealth}\n" +
-            $"Don't get hit by any abilities: {!isHitBySkill}";
+            $"Don't get hit by any abilities: {!isHitBySkill}\n" +
+            $"Rating: {rating}\n";
     }
 }
--- Assets/Scripts/units&levels/levels/Level 3/lvl3_ObjTracker.cs
+++ Assets/Scripts/units&levels/levels/Level 3/lvl3_ObjTracker.cs
@@ -14,10 +14,38 @@ public class Lvl3_ObjTracker : GameObjectiveTracker
 
     public override string ParseObjectiveText()
     {
+        int score = 0;
+        string rating;
+        if (IsKillThreeGuard()) score += 35;
+        if (!isHealBoss) score += 20;
+        if (!isBishopExplode) score += 20;
+        score += player.currentHealth;
+
+        switch (score)
+        {
+            case >= 141:
+                rating = "S";
+                break;
+            case >= 120:
+                rating = "A";
+                break;
+            case >= 100:
+                rating = "B";
+                break;
+            case >= 70:
+                rating = "C";
+                break;
+            default:
+                rating = "D";
+                break;
+
+        }
+
         return $"Player Hits: {playerHits}\n" +
             $"Player HP: {player.currentHealth}/{player.maxHealth}\n" +
             $"Kill 3 living guards/bishops within 10 sec: {IsKillThreeGuard()}\n" +
             $"No heal boss: {!isHealBoss}\n" +
-            $"No exploding bishop: {!isBishopExplode}";
+            $"No exploding bishop: {!isBishopExplode}\n" +
+            $"Rating: {rating}\n";
     }
 }
